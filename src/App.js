5cc4dde16cda82213d3f1795505883bdc0be35c5
Thales Gava <63215546+thalesgava@users.noreply.github.com>
2020-11-13 20:42:09 -0200

    components resumo e consultas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import './App.css';
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
 import Resumo from './components/Resumo/Resumo';
+import Consultas from './components/Consultas/Consultas';
 
 class App extends Component {
   render() {
      return (
      <div>
          <MenuSuperior/>
-         <Resumo/>  
+         <Resumo/>
+         <Consultas/>
      </div>
      );
   }
